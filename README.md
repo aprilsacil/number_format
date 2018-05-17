36748b2a0ce656ae804b0de935128547f3567995
aprilsacil <aprilvsacil@gmail.com>
2018-05-17 11:37:23 +0800

    number_format jquery

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # number_format
-jQuery number_format, Javascript number_format
+
+jQuery/Javascript equivalent of number_format in PHP
+
+## Usage
+
+### Direct formatting
+1. basic
+    ```javascript
+    $.number_format('12345.6789'); // Outputs 12,345
+    ```
+2. decimal places -- default is 0
+    ```javascript
+    $.number_format('12345.6789', 2); // Outputs 12,345.68
+    ```
+3. decimal separator -- default is '.'
+    ```javascript
+    $.number_format('12345.6789', 2, 'xx'); // Outputs 12,345xx68
+    ```
+4. thousands separator -- default is ','
+    ```javascript
+    $.number_format('123456789', 0, '.', ','); // Outputs 123,456,789
+    ```
+### Element formatting
+assuming we have:
+    ```html
+    <div class="prices">
+      <span class="price">12434.445</span>
+      <span class="price">123434545</span>
+      <span class="price">12345.7834893748973</span>
+      <span class="price">15.56</span>
+    </div>
+    ```
+the script:
+    ```javascript
+        $('.price').number_format(2, '.', ',');
+    ```
+could convert it to:
+    ```html
+    <div class="prices">
+      <span class="price">12,434.45</span>
+      <span class="price">123,434,545</span>
+      <span class="price">12,345.78</span>
+      <span class="price">15.56</span>
+    </div>
+    ```
---
+++ number_format.js
@@ -0,0 +1,88 @@
+/**
+ * number_format - javascript/jquery equivalent of number_format in php with additional abilities
+ *
+ * @version 0.0.1
+ * @author April Sacil <aprilvsacil@gmail.com>
+ * @website https://github.com/aprilsacil/number_format
+ * @license MIT
+ */
+/*
+ * $(element).number(2, '.', ',');
+ */
+jQuery.fn.extend({
+    number_format: function (decimalLength, decimal, separator) {
+         if (typeof separator == 'undefined') {
+             separator = ',';
+         }
+
+         if (typeof decimal == 'undefined') {
+             decimal = '.';
+         }
+
+         if (typeof decimalLength == 'undefined') {
+             decimalLength = 2;
+         }
+         return this.each(function () {
+            var elem = $(this);
+            var cents = '';
+            var factor = Math.pow(10, decimalLength);
+            var number = $.trim(elem.html()).split('.');
+
+            var integer = number[0];
+            if (decimalLength
+                && number.length > 1
+                && number[1]
+            ) {
+                number[1] = 0 + '.' + number[1];
+                cents = Math.round(number[1] * factor) / factor;
+                cents = !cents ? '' : cents.toString().replace('0.', decimal);
+            }
+            var regex = /(\d+)(\d{3})/;
+            while (regex.test(integer)) {
+                integer = integer.replace(regex, '$1' + separator + '$2');
+            }
+
+            elem.html(integer + cents);
+         });
+     }
+});
+
+/*
+ * $.number('5125.123', 2, '.', ',');
+ */
+jQuery.extend({
+    number_format: function (number, decimalLength, decimal, separator) {
+        var cents = '';
+        if (typeof separator == 'undefined') {
+            separator = ',';
+        }
+
+        if (typeof decimal == 'undefined') {
+            decimal = '.';
+        }
+
+        if (typeof decimalLength == 'undefined') {
+            decimalLength = 0;
+        }
+
+        var factor = Math.pow(10, decimalLength);
+        var number = number.split('.');
+        var integer = number[0];
+
+        if (decimalLength
+            && number.length > 1
+            && number[1]
+        ) {
+            number[1] = 0 + '.' + number[1];
+            cents = Math.round(number[1] * factor) / factor;
+            cents = !cents ? '' : cents.toString().replace('0.', decimal);
+        }
+
+        var regex = /(\d+)(\d{3})/;
+        while (regex.test(integer)) {
+            integer = integer.replace(regex, '$1' + separator + '$2');
+        }
+
+        return integer + cents;
+    }
+});
